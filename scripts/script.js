6348ad2348abb9e618a5ec4800746ea927467d54
Souhailabachoukh <souhaila.bachoukh@hva.nl>
2025-09-20 23:54:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 console.log("hi");
 
+/*hamburgermenu*/
 const menuButton = document.querySelector("header li button:first-of-type")
 const navMenu = document.querySelector("header nav")
 
@@ -12,7 +13,7 @@ function toonMenu () {
 }
 
 
-
+/*hamburgermenu sluiten*/
 const sluitButton = document.querySelector("nav button")
 
 sluitButton.addEventListener("click", sluitMenu);
@@ -23,7 +24,7 @@ function sluitMenu () {
 }
 
 
-
+/*hamburgersubmenu*/
 const subMenuButton = document.querySelector("nav ul:first-of-type li:nth-of-type(4) a")
 const navSubMenu = document.querySelector("nav ul:nth-of-type(2)")
 
@@ -33,3 +34,8 @@ function toonSubMenu () {
   console.log("submenu geopend")
   navSubMenu.classList.toggle("toonSubMenu");
 }
+
+/*header div*/
+
+
+
